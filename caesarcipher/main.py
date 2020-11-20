acd3457963cd431672374dc13a0fc27c41d85a0f
s-kngstn <s.kngstn@protonmail.com>
2020-11-20 17:00:51 +0000

    caesar cipher added

---
+++ caesarcipher/main.py
@@ -0,0 +1,35 @@
+from art import logo
+
+print(logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+def start():
+  def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+      shift_amount *= -1
+    for char in start_text:
+      if char not in alphabet:
+        end_text += char
+      else:
+        position = alphabet.index(char)
+        new_position = position + shift_amount
+        end_text += alphabet[new_position]
+      
+    print(f"Here's the {cipher_direction}d result: {end_text}")
+
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+  #print(shift)
+  shift = shift % 25
+  #print(shift)
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  start_over = input("Would you like to start over?\n").lower()
+  if start_over == 'yes':
+    start()
+  else:
+    print("Goodbye.")
+
+start()
+
