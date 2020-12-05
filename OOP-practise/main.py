27eb163a5b1d35dbd2c82a232a9040e69bb8ddd2
s-kngstn <s.kngstn@protonmail.com>
2020-12-05 19:17:24 +0000

    added oop version of the coffee machine

---
+++ OOP-practise/main.py
@@ -0,0 +1,28 @@
+# import another_module
+
+# print(another_module.another_variable)
+
+
+# from turtle import Turtle, Screen
+
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("chartreuse3")
+# timmy.forward(100)
+
+# my_screen = Screen()
+# print(my_screen.canvwidth)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+
+
+print(table)
+
+# x.add_column("City name",
+# ["Adelaide","Brisbane","Darwin","Hobart","Sydney","Melbourne","Perth"])
