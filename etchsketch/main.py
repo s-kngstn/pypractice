0206848ade42d8605175b3ae424bcb10df871426
s-kngstn <s.kngstn@protonmail.com>
2020-12-18 06:43:44 +0000

    completed turtle race

--- etchsketch/main.py
+++ etchsketch/main.py
@@ -1,34 +1,39 @@
 from turtle import Turtle, Screen
+import random
 
-tim = Turtle()
+is_race_on = False
 screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
 
 
-def move_forwards():
-    tim.forward(10)
+for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    new_turtle.color(colors[turtle_index])
+    all_turtles.append(new_turtle)
 
-def move_backwards():
-    tim.backward(10)
+if user_bet:
+    is_race_on = True
 
-def move_left():
-    tim.left(10)
+while is_race_on:
 
-def move_right():
-    tim.right(10)
+    for turtle in all_turtles:
+        if turtle.xcor() > 370:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You won the bet, congrats on picking the {winning_color} turtle")
+            else:
+                print(f"You lost the bet, the {winning_color} turtle is the winner!")
 
-def clear_screen():
-    tim.clear()
-    tim.penup()
-    tim.home()
-    tim.pendown()
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
 
-screen.listen()
-screen.onkey(key="c", fun=clear_screen)
-screen.onkey(key="a", fun=move_left)
-screen.onkey(key="d", fun=move_right)
-screen.onkey(key="w", fun=move_forwards)
-screen.onkey(key="s", fun=move_backwards)
-screen.exitonclick()
 
 
 
