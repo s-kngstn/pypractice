42407438dc9d90b0e01d01a07e6ddd3357ba1920
s-kngstn <s.kngstn@protonmail.com>
2020-12-22 10:52:28 +0000

    created port scanner for pen testing

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,8 +1,8 @@
+import time
 from turtle import Screen
 from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
-import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -20,8 +20,8 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-game_is_on = True
-while game_is_on:
+GAME_IS_ON = True
+while GAME_IS_ON:
     screen.update()
     time.sleep(0.1)
 
@@ -29,22 +29,22 @@ while game_is_on:
 
     snake.move()
 
-    #Detect collision with food
+    # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
         score.add_point()
-    
-    #Detect collision with wall
-    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-        game_is_on = False
+
+    # Detect collision with wall
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor(
+    ) > 290 or snake.head.ycor() < -290:
+        GAME_IS_ON = False
         score.game_over()
 
-    #Detect collision with tail
+    # Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
+            GAME_IS_ON = False
             score.game_over()
-
 
 screen.exitonclick()
