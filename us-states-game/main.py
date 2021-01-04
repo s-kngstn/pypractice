10e21836d8bbdab22206f1f0be6714ffba8e7dc4
s-kngstn <s.kngstn@protonmail.com>
2021-01-04 16:12:18 +0000

    modified us-states-game 2 inc list comprehension

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -18,7 +18,6 @@ states_found_list = []
 # Game Score
 correct_states = 1
 
-
 states = turtle.Turtle()
 states.hideturtle()
 states.penup()
@@ -30,7 +29,9 @@ score.goto(250, 250)
 
 GAME_IS_ON = True
 while GAME_IS_ON:
-    answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?").title()
+    answer_state = screen.textinput(
+        title="Guess the State",
+        prompt="What's another state's name?").title()
     location = state_data[state_data.state == answer_state]
 
     if answer_state == "Exit":
@@ -42,21 +43,26 @@ while GAME_IS_ON:
         states.goto(X, Y)
         states.write(f"{answer_state}", move=False, align="center", font=FONT)
         score.clear()
-        score.write(f"{correct_states}/50 States Correct", move=False, align="center", font=FONT)
+        score.write(f"{correct_states}/50 States Correct",
+                    move=False,
+                    align="center",
+                    font=FONT)
         correct_states += 1
         if len(states_found_list) == 50:
             score.clear()
             score.goto(50, 250)
-            score.write("Congratulations! You Found All 50 States! Click Map To Exit", move=False, align="center", font=FONT)
+            score.write(
+                "Congratulations! You Found All 50 States! Click Map To Exit",
+                move=False,
+                align="center",
+                font=FONT)
             GAME_IS_ON = False
             screen.exitonclick()
     else:
         pass
 
-states_to_learn = []
-for state in state_list:
-    if state not in states_found_list:
-        states_to_learn.append(state)
-
+states_to_learn = [
+    state for state in state_list if state not in states_found_list
+]
 state_dataframe = pandas.DataFrame(states_to_learn)
 state_dataframe.to_csv("states_to_learn.csv")
