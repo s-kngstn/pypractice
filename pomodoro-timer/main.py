8225b5041190e2fab4c68557f8417004b9a8c406
s-kngstn <s.kngstn@protonmail.com>
2021-01-08 20:36:47 +0000

    worked on logic for pomodoro, finishing tomorrow

--- pomodoro-timer/main.py
+++ pomodoro-timer/main.py
@@ -10,26 +10,57 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+REPS = 0
+timer = None
+
 
 # ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    window.after_cancel(timer)
+    timer_text.config(text="00:00")  # <- wrong watch last video and check
+    # title_label "Timer"
+    # reset check_marks
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
 
 def start_clicked():
-
-    count_down(5 * 60)
+    global REPS
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    REPS += 1
+    print(REPS)
+    if REPS % 8 == 0:
+        count_down(long_break_sec)
+        title.config(text="Break", fg=RED)
+    elif REPS % 2 == 0:
+        count_down(short_break_sec)
+        title.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        title.config(text="Work", fg=GREEN)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
-
+    global timer
     count_min = math.floor(count / 60)
     count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_clicked()
+        marks = ""
+        work_sessions = math.floor(REPS / 2)
+        for _ in range(work_sessions):
+            marks += " ✔"
+        checkmarks.config(text=marks)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -68,13 +99,11 @@ start = tkinter.Button(text="Start",
 start.grid(column=0, row=2)
 
 # Reset Button
-reset = tkinter.Button(text="Reset",
-                       command=start_clicked,
-                       highlightthickness=0)
+reset = tkinter.Button(text="Reset", command=reset_timer, highlightthickness=0)
 reset.grid(column=2, row=2)
 
 # Checkmark
-checkmarks = tkinter.Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20))
+checkmarks = tkinter.Label(fg=GREEN, bg=YELLOW)
 checkmarks.grid(column=1, row=3)
 
 window.mainloop()
