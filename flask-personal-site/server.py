c0ae8a5eaab63ccc4bc67cc0c71a5cc6d4bdf7b2
s-kngstn <s.kngstn@protonmail.com>
2021-02-22 08:06:57 +0000

    using web templates in flask

---
+++ flask-personal-site/server.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('chad.html')
+    
+
+if __name__ == "__main__":
+    #Run the app in debug mode to auto-reload
+    app.run(debug=True)
