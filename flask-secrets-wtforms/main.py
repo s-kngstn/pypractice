51f87b9c5e47652529570558e4768e2022f80618
s-kngstn <s.kngstn@protonmail.com>
2021-03-11 18:40:08 +0000

    wtforms and flask-bootstrap

---
+++ flask-secrets-wtforms/main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+app = Flask(__name__)
+app.secret_key = "secret-key"
+Bootstrap(app)
+
+
+class MyForm(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField(label='Log In')
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = MyForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@email.com" and form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template('login.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
